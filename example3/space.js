7a92a22acec91559e2eca1644a39785c9775f413
Paul Bonser <pib@paulbonser.com>
2011-01-05 21:56:14 -0600

    functioning gravity, separated acceleration loop from movement loop so planets after the first don't have their acceleration incorrectly calculated from the new position of other planets

--- example3/space.js
+++ example3/space.js
@@ -1,3 +1,20 @@
+function debug(field, val) {
+    var dbg_elem = document.getElementById('debug');
+    if (!dbg_elem) {
+        dbg_elem = document.createElement('DIV');
+        dbg_elem.id = 'debug';
+        document.getElementsByTagName('body')[0].appendChild(dbg_elem);
+    }
+
+    debug.data[field] = val;
+    var debug_data = '<table>';
+    for (var name in debug.data) {
+        debug_data += '<tr><td>' + name + ' :</td><td class="data">' + debug.data[name] + '</td></tr>';
+    }
+    dbg_elem.innerHTML = debug_data;
+}
+debug.data = {};
+
 var universe = {
     "G": 6.67428e-11,
     "time_multiplier": 60 * 60 * 24,
@@ -7,19 +24,27 @@ var universe = {
         {
             "name": "Terra",
             "color": "#0000FF",
-            "radius": 6371,
+            "radius": 6371.0,
             "mass": 5.9736e+24,
             "position": [0, 0],
-            "velocity": [0, 0]
+            "velocity": [0, -0.0125]
         },
         {
             "name": "Luna",
             "color": "#FFFFFF",
             "radius": 1737.10,
             "mass": 7.3477e+22,
-            "position": [363104, 0],
+            "position": [363104.0, 0],
             "velocity": [0, 1.022]
-        }
+        }/*,
+        {
+            "name": "Luna2",
+            "color": "#FFFF00",
+            "radius": 1737.10,
+            "mass": 7.3477e+22,
+            "position": [-363104.0, 0],
+            "velocity": [-0.0, -1.022]
+        }*/
     ]
 };
 
@@ -50,13 +75,25 @@ function draw_body(ctx, body) {
     ctx.restore();
 }
 
+var first = true;
 function draw_universe(ctx, universe) {
     ctx.save();
-    ctx.fillStyle = '#000000';
+    if (first) {
+        ctx.fillStyle = 'rgba(0,0,0, 1)';
+        ctx.fillRect(0, 0, ctx.width, ctx.height);
+        first = false;
+    }
+    ctx.fillStyle = 'rgba(0,0,0, .004)';
     ctx.fillRect(0, 0, ctx.width, ctx.height);
 
     ctx.scale(ctx.width / universe.width, ctx.height / universe.height);
     ctx.translate(universe.width / 2, universe.height / 2);
+
+    ctx.strokeStyle = '#ffffff';
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(360000000, 360000000);
+    ctx.stroke();
 
     for_each_body(universe, function(body) {
         draw_body(ctx, body);
@@ -85,15 +122,20 @@ function v2d_length(v) {
 }
 
 function gravitation(universe, attractor, attractee) {
-    var dr = v2d_sub(attractor, attractee);
-    var r = v2d_length(dr);
-    var d = v2d_div(dr, r);
-    var g = -(universe.G * attractor.mass) / (r * r);
-    return v2d_mult(d, g);
+    var distance_vector = v2d_sub(attractor.position, attractee.position);
+    var distance_km = v2d_length(distance_vector);
+    var direction = v2d_div(distance_vector, distance_km);
+    // positions are stored in kilometers, so we have to convert to meters
+    var distance_m = distance_km * 1000;
+    var g = (universe.G * attractor.mass) / (distance_m * distance_m);
+    // velocities are also in km/s, so this needs to be scaled back down
+    return v2d_mult(direction, g / 1000);
 }
 
 function step_universe(dt, universe) {
     for_each_body(universe, function(body) {
+        debug(body.name + '.x', body.position[0].toFixed(2));
+        debug(body.name + '.y', body.position[1].toFixed(2));
         // gravitate
         for_each_body(universe, function(sub_body) {
             if (sub_body != body) {
@@ -101,6 +143,8 @@ function step_universe(dt, universe) {
                 body.velocity = v2d_add(body.velocity, v2d_mult(g, dt));
             }
         });
+    });
+    for_each_body(universe, function(body) {
         body.position = v2d_add(body.position,
                                 v2d_mult(body.velocity, dt));
     });
